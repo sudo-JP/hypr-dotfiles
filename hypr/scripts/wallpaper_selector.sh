4567c5c114524be9c66dce5d74c8cc9374ebf8fd
Jason Phan <jase.phan@mail.utoronto.ca>
2025-08-13 14:06:41 -0400

    added wallpaper picker and more bg images

--- hypr/scripts/wallpaper_selector.sh
+++ hypr/scripts/wallpaper_selector.sh
@@ -30,5 +30,8 @@ ln -sf $WALLPAPER_DIR/link $SYMLINK_PATH
 
 pkill swaybg
 hyprctl dispatch -- exec swaybg -m fill -i "$SYMLINK_PATH"
+magick "$SYMLINK_PATH" -resize 2300x1294\> -quality 85 "$HOME/.config/rofi/wallpaper.jpg"
+cp "$HOME/.config/rofi/wallpaper.jpg" "$HOME/.config/wlogout/wallpaper.jpg"
+magick "$HOME/.config/wlogout/wallpaper.jpg" -blur 0x8 ~/.config/wlogout/wallpaper.jpg
 
 
